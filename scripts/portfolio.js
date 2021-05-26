e409bac2ef4eb9c8af60fbd8500d72809af74995
양희연 <deepbm27@gmail.com>
2021-05-26 11:51:09 +0900

    Make navbar

---
+++ scripts/portfolio.js
@@ -0,0 +1,8 @@
+/* contact me */
+$('.btn-contact').mouseover(() => {
+  $('.btn-contact').html('deepbm27@gmail.com');
+});
+
+$('.btn-contact').mouseleave(() => {
+  $('.btn-contact').html('Contact me');
+});
