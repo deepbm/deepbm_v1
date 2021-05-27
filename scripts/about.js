2865e72ca356770cd8872004d328e94f5bfc8f20
양희연 <deepbm27@gmail.com>
2021-05-27 17:01:04 +0900

    Refactor for code recycle

--- scripts/about.js
+++
@@ -1,8 +0,0 @@
-/* contact me */
-$('.btn-contact').mouseover(() => {
-  $('.btn-contact').html('deepbm27@gmail.com');
-});
-
-$('.btn-contact').mouseleave(() => {
-  $('.btn-contact').html('Contact me');
-});
