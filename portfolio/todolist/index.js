797ab3c828e3165f06f8587b56f13e506d4df888
양희연 <deepbm27@gmail.com>
2021-06-04 20:22:47 +0900

    Make portfolio-todolist append event

--- portfolio/todolist/index.js
+++ portfolio/todolist/index.js
@@ -18,3 +18,32 @@ $('.to-do-list').click(e => {
     $(e.target).parents('li').remove();
   }
 });
+
+
+// append
+function add() {
+  const value = $('.input-append').val();
+
+  if (value) {
+    $('.to-do-list').append(`<li>
+    <i class="far fa-square checkbox"></i>
+    <span class="item">${value}</span>
+    <button class="btn btn-remove"><i class="fas fa-trash"></i></button>
+  </li>`)
+    $('.input-append').val('');
+  } else {
+    alert('Please input a text!');
+  }
+}
+
+// enter key
+$('.input-append').keydown(key => {
+  if (key.keyCode === 13) {
+    add();
+  }
+});
+
+// ADD buttton
+$('.btn-append').click(() => {
+  add();
+});
