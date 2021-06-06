55cc9da05e523884d3dff213011f6566b48f92b6
양희연 <deepbm27@gmail.com>
2021-06-06 20:10:29 +0900

    Create portfolio-pick menu data and improve remove event

--- portfolio/pick/index.js
+++ portfolio/pick/index.js
@@ -1,8 +1,18 @@
-// list
-$('.menu-list').click(e => {  
-  // remove
-  if (e.target.classList.contains('btn-remove') || e.target.classList.contains('fa-trash')) {
-    $(e.target).parents('li').remove();
+// menu data
+let menus = [];
+
+
+// remove
+const menuList = document.querySelector('.menu-list');
+menuList.addEventListener('click', e => {
+  if (e.target.classList.contains('btn-remove')) {
+    const removeMenu = e.target.previousElementSibling.innerHTML;
+    menus.splice(menus.indexOf(removeMenu), 1);
+    e.target.parentElement.remove();
+  } else if (e.target.classList.contains('fa-trash')) {
+    const removeMenu = e.target.parentElement.previousElementSibling.innerHTML;
+    menus.splice(menus.indexOf(removeMenu), 1);
+    e.target.parentElement.parentElement.remove();
   }
 });
 
@@ -10,6 +20,7 @@ $('.menu-list').click(e => {
 // append
 function add() {
   const value = $('.input-append').val();
+  menus.push(value);
 
   if (value) {
     $('.menu-list').append(`<li>
