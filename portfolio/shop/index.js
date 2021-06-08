051354905b5b9a59f721cb56b31d1cc56640d6df
양희연 <deepbm27@gmail.com>
2021-06-08 14:19:09 +0900

    Make portfolio-shop get data from ajax

--- portfolio/shop/index.js
+++ portfolio/shop/index.js
@@ -46,3 +46,36 @@ $(window).scroll(() => {
 $('.btn-top').click(() => {
   $(window).scrollTop(0);
 });
+
+
+// 상품 더보기
+$('.btn-more').click(() => {
+  $('.btn-more').hide();
+  $('.loader').show();
+  let loading = setTimeout(dataLoading, 2000);
+});
+
+function dataLoading() {
+  $.ajax({
+    url: 'https://deepbm.github.io/data/data.json',
+    method: 'GET',
+    dataType: 'json'
+  }).done(response => {
+    $('.loader').hide();
+    const count = response.length;
+    for (let i = 0; i < count; i++) {
+      const product = `<div class="col-12 col-md-6 col-lg-4">
+    <div class="product">
+      <img class="w-100" src="${response[i].image}" alt="fedora_hat">
+      <div class="product-info">
+        <p class="mt-3 mb-0">${response[i].product}</p>
+        <p class="mt-1">${response[i].price}</p>
+      </div>
+    </div>
+  </div>`;
+      $('.row').append(product);
+    }
+  }).fail(() => {
+    alert('새로고침 후 다시 시도해주세요.');
+  });
+}
