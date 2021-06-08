6a01a3e68f6a2cb29fbb0e8885184150520efbf0
양희연 <deepbm27@gmail.com>
2021-06-08 11:44:33 +0900

    Make portfolio-shop top button UI and event

--- portfolio/shop/index.js
+++ portfolio/shop/index.js
@@ -32,3 +32,17 @@ $('form').on('submit', e => {
     $('.alert-pw').show();
   }
 });
+
+
+// top button
+$(window).scroll(() => {
+  if ($(window).scrollTop() > 200) {
+    $('.btn-top').fadeIn();
+  } else {
+    $('.btn-top').fadeOut();
+  }
+});
+
+$('.btn-top').click(() => {
+  $(window).scrollTop(0);
+});
